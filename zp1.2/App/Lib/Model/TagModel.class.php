575bd297f9cff7637fc47b8be2ac46624a8a1562
xuwenmin <151481184@qq.com>
2013-05-13 10:49:09 +0800

    新增作品标签关联删除

--- zp1.2/App/Lib/Model/TagModel.class.php
+++ zp1.2/App/Lib/Model/TagModel.class.php
@@ -1,5 +1,16 @@
 ﻿<?php
 // 标签模型
-class TagModel extends CommonModel {
+class TagModel extends RelationModel {
+	// 自动验证设置
+	protected $_validate         =         array(
+		array('tagname','require','标签名称必需填写！',1)
+		);
+	protected $_link = array(
+		'Tag_relationship' => array(
+			'mapping_type' => HAS_MANY,
+			'class_name'   =>'Tag_relationship',
+			'foreign_key' => 'tagid',
+			)
+		);
 }
 ?>
