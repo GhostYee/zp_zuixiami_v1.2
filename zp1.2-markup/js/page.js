77441bd3fbba51a1be9a995f18f189b0ed51b936
zmyfujian <87651357@qq.com>
2013-05-17 15:28:47 +0800

    修改
    1.修改作品列表显示项目
    2.修改样式
    3.合并动作样式
    4.合并js
    5.首页增加标签
    6.修改php程序  首页部分

--- zp1.2-markup/js/page.js
+++ zp1.2-markup/js/page.js
@@ -1,12 +1,19 @@
+$(document).ready(function() {
+	initPageBanner();
+	theHomePageAni();
+});
+
+
 /***** code by perkyliu@126.com *****/
-function initPageBanner(){
+
+function initPageBanner() {
 	var focusScroll = new ScrollPic();
 	focusScroll.scrollContId = "page-banner"; //内容容器ID
-	focusScroll.dotClassName = "";//点className
-	focusScroll.dotOnClassName = "cur";//当前点className
-	focusScroll.listType = "";//列表类型(number:数字，其它为空)
+	focusScroll.dotClassName = ""; //点className
+	focusScroll.dotOnClassName = "cur"; //当前点className
+	focusScroll.listType = ""; //列表类型(number:数字，其它为空)
 	focusScroll.listEvent = "onmouseover"; //切换事件
-	focusScroll.frameWidth = 494;//显示框宽度
+	focusScroll.frameWidth = 494; //显示框宽度
 	focusScroll.pageWidth = 494; //翻页宽度
 	focusScroll.upright = false; //垂直滚动
 	focusScroll.speed = 10; //移动速度(单位毫秒，越小越快)
@@ -16,17 +23,58 @@ function initPageBanner(){
 	focusScroll.circularly = true;
 	focusScroll.initialize(); //初始化
 
-	$('#page-btn-prev').click(function(){
-	  focusScroll.pre();
-	  return false;
-	}) 
+	$('#page-btn-prev').click(function() {
+		focusScroll.pre();
+		return false;
+	})
 
-	$('#page-btn-next').click(function(){
-	    focusScroll.next();
-	    return false;
+	$('#page-btn-next').click(function() {
+		focusScroll.next();
+		return false;
 	})
 }
 
-$(document).ready(function() {
-	initPageBanner();
-});
+/***** code by zmy *****/
+
+function theHomePageAni() {
+	if(getCookie("rainbow-ani")=="")
+	{
+		$(".wgt-rainbow").parent().addClass("rainbow-ani");
+		setCookie("rainbow-ani","yet");
+	}
+}
+
+//获得coolie 的值
+
+function getCookie(name) {
+	var cookieArray = document.cookie.split("; "); //得到分割的cookie名值对  
+	var cookie = new Object();
+	for (var i = 0; i < cookieArray.length; i++) {
+		var arr = cookieArray[i].split("="); //将名和值分开   
+		if (arr[0] == name) return unescape(arr[1]); //如果是指定的cookie，则返回它的值 
+	}
+	return "";
+}
+
+function delCookie(name) {
+	document.cookie = name + "=;expires=" + (new Date(0)).toGMTString();
+}
+
+function addCookie(objName, objValue, objHours) { //添加cookie
+	var str = objName + "=" + escape(objValue);
+	if (objHours > 0) { //为时不设定过期时间，浏览器关闭时cookie自动消失
+		var date = new Date();
+		var ms = objHours * 3600 * 1000;
+		date.setTime(date.getTime() + ms);
+		str += "; expires=" + date.toGMTString();
+	}
+	document.cookie = str;
+}
+
+function setCookie(name, value) {
+	var Days = 30; //此 cookie 将被保存 30 天
+	var exp = new Date(); //new Date("December 31, 9998");
+	exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+	document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+
+}
