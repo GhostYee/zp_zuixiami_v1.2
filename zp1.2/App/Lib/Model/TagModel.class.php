8780f798c68bf40f398d9cec94e26045d8d47e01
wei5634 <wei5634@126.com>
2013-05-22 22:36:36 +0800

    修改首页tag

--- zp1.2/App/Lib/Model/TagModel.class.php
+++ zp1.2/App/Lib/Model/TagModel.class.php
@@ -12,5 +12,16 @@ class TagModel extends CommonModel {
 			'foreign_key' => 'tagid',
 			)
 		);
+	public function getWorksByTagID($tagid){
+		$sql    = "SELECT w.*,IFNULL(author,qm.name) author,s.name sortname,qs.name qunname,qm.id".
+				" author_id FROM ".C('DB_PREFIX')."works w ".
+				" LEFT JOIN ".C('DB_PREFIX')."works_sort s ON s.id=w.sortid ".
+				" LEFT JOIN ".C('DB_PREFIX')."qun_sort qs ON qs.id=w.qun_sortid ".
+				" LEFT JOIN ".C('DB_PREFIX')."qun_member qm ON qm.qq=w.qq ".
+				" LEFT JOIN ".C('DB_PREFIX')."tag_relationship tr on tr.workid=w.id ".
+				" WHERE w.status=2 AND tr.tagid=$tagid order by w.id desc ";
+		$works  = $this->query($sql);
+		return $works;
+	}
 }
 ?>
