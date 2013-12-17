326b9738f470eb7a91266779858bce215451368f
cuixiping <cuixiping@gmail.com>
2013-12-17 13:07:13 +0800

    SEO meta信息补全

--- zp1.2/App/Conf/Home/config.php
+++ zp1.2/App/Conf/Home/config.php
@@ -6,5 +6,8 @@ return array(
 	
 	'TMPL_ACTION_ERROR'         =>  'Public:success', // 默认错误跳转对应的模板文件
 	'TMPL_ACTION_SUCCESS'       =>  'Public:success', // 默认成功跳转对应的模板
+	'CFG_SEO_TITLE'       =>  '最虾米，发现前端价值',
+	'CFG_SEO_KEYWORDS'    =>  '最虾米,Web前端开发,移动Web开发,Javascript,CSS,HTML5,CSS3',
+	'CFG_SEO_DESCRIPTION' =>  '最虾米,技术氛围最好的Web前端开发交流区'
 );
 ?>
