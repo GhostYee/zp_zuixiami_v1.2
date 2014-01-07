be97a7bf30770ba72b5889826136cfa18ed80821
zmyfujian <87651357@qq.com>
2014-01-07 17:24:27 +0800

    统一样式，

--- zp1.2/tempProject/inc/header.php
+++ zp1.2/tempProject/inc/header.php
@@ -5,8 +5,8 @@
 <head>
 <meta charset="utf-8">
 <title>最虾米-发现前端价值</title>
-<link href="../asset/css/style-1.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="../../App/Tpl/Home/default/Public/css/style-1.css" media="screen" rel="stylesheet" type="text/css" />
 <!--[if lt IE 9]>
-<script type="text/javascript" src="asset/js/html5.js" ></script>
+<script type="text/javascript" src="../../App/Tpl/Home/default/Public/js/html5.js" ></script>
 <![endif]-->
 </head>
